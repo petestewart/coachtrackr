1f0b7cd8013e26c297df63299127d0faa255aafd
Pete Stewart <petestew@gmail.com>
2021-02-28 20:25:09 -0600

    sidenav bones created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import HomeIcon from "@material-ui/icons/Home";
+// import ReceiptIcon from "@material-ui/icons/Receipt";
+// import NotificationsIcon from "@material-ui/icons/Notifications";
+// import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
+// import SettingsIcon from "@material-ui/icons/Settings";
+
+import Layout from './components/Layout';
+// import Sidebar from "./Sidebar/XXSidebar";
+
+// const onClick = (e, item) => {
+//   window.alert(JSON.stringify(item, null, 2));
+// }
+
+// const items = [
+//   { name: "home", label: "Home", Icon: HomeIcon },
+//   {
+//     name: "billing",
+//     label: "Billing",
+//     Icon: ReceiptIcon,
+//     items: [
+//       { name: "statements", label: "Statements", onClick, 
+//       Icon: ReceiptIcon,},
+//       { name: "reports", label: "Reports", onClick }
+//     ]
+//   },
+//   "divider",
+//   {
+//     name: "settings",
+//     label: "Settings",
+//     Icon: SettingsIcon,
+//     items: [
+//       { name: "profile", label: "Profile" },
+//       { name: "insurance", label: "Insurance", onClick },
+//       "divider",
+//       {
+//         name: "notifications",
+//         label: "Notifications",
+//         Icon: NotificationsIcon,
+//         items: [
+//           { name: "email", label: "Email", onClick },
+//           {
+//             name: "desktop",
+//             label: "Desktop",
+//             Icon: DesktopWindowsIcon,
+//             items: [
+//               { name: "schedule", label: "Schedule" },
+//               { name: "frequency", label: "Frequency" }
+//             ]
+//           },
+//           { name: "sms", label: "SMS" }
+//         ]
+//       }
+//     ]
+//   }
+// ];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout />
+      {/* <Sidebar items={items} /> */}
     </div>
   );
 }
